3bb3b6ede38dc7a16333c0ba98e3999bb02de061
João Vieira <jpedrovieira@ipvc.pt>
2021-05-17 22:28:15 +0100

    dasd

--- routes.js
+++ routes.js
@@ -3,5 +3,8 @@ const routes = express.Router();
 const analises = require("./src/controllers/teste");
 
 routes.get('/api/teste',analises.getReviewById)
+routes.get('/',async function(req, res){
+    return res.json("db connection done");
+})
 
 module.exports = routes;
