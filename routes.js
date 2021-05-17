43d69f92885b324538d0b45355ab8c9d8a4b4b48
João Vieira <jpedrovieira@ipvc.pt>
2021-05-17 22:31:11 +0100

    asdas

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const analises = require("./src/controllers/teste");
 
-routes.get('/api/teste',analises.getReviewById)
+//routes.get('/api/teste',analises.getReviewById)
 routes.get('/',async function(req, res){
     return res.json("db connection done");
 })
