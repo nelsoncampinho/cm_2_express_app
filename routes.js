478f199a41f2ffc834a2451dcaa98bc111676280
João Vieira <jpedrovieira@ipvc.pt>
2021-05-17 22:45:54 +0100

    adsdsa

--- routes.js
+++ routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const routes = express.Router();
 const analises = require("./src/controllers/teste");
 
-//routes.get('/api/teste',analises.getReviewById)
+routes.get('/api/teste',analises.getReviewById)
 routes.get('/',async function(req, res){
-    return res.json("db connection done");
+    return res.json(process.env.DB_HOST);
 })
 
 module.exports = routes;
