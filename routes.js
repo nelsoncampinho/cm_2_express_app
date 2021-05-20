6a42bdbdde39f10a981e51239e35c2575728b83b
João Vieira <jpedrovieira@ipvc.pt>
2021-05-20 18:56:40 +0100

    fixed routes

--- routes.js
+++ routes.js
@@ -4,9 +4,9 @@ const login = require("./src/controllers/login");
 const users = require("./src/controllers/users");
 const company = require("./src/controllers/company")
 
-routes.post('/api/login',login.login)
-routes.post('/api/employee-info',users.get_employee_info)
-routes.post('/api/company-info',users.get_company_info)
+routes.post('/api/users/login',login.login)
+routes.post('/api/employee/info',users.get_employee_info)
+routes.post('/api/company/info',users.get_company_info)
 routes.post('/api/company/vehicle',company.get_company_vehicle)
 
 
