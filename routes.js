c8695810b1ba9c663c5f5e52f75cbd5992ced04d
João Vieira <jpedrovieira@ipvc.pt>
2021-05-20 19:45:24 +0100

    asdasd

--- routes.js
+++ routes.js
@@ -4,10 +4,10 @@ const login = require("./src/controllers/login");
 const users = require("./src/controllers/users");
 const company = require("./src/controllers/company")
 
-routes.post('/api/users/login/',login.login)
-routes.post('/api/employee/info/',users.get_employee_info)
-routes.post('/api/company/info/',users.get_company_info)
-routes.post('/api/company/vehicle/',company.get_company_vehicle)
+routes.post('/api/users/login',login.login)
+routes.post('/api/employee/info',users.get_employee_info)
+routes.post('/api/company/info',users.get_company_info)
+routes.post('/api/company/vehicle',company.get_company_vehicle)
 
 
 routes.get('/',async function(req, res){
