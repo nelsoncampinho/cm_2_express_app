4692443bedd27e7c361acf4f2a60112cd69d95bf
João Vieira <jpedrovieira@ipvc.pt>
2021-05-20 15:58:12 +0100

    endpoint login done

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const routes = express.Router();
-const analises = require("./src/controllers/teste");
+const login = require("./src/controllers/login");
 
-routes.get('/api/teste',analises.getReviewById)
+routes.post('/api/login',login.login)
 routes.get('/',async function(req, res){
     return res.json("Welcome to API");
 })
---
+++ src/controllers/login.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const connection = require("../db/config");
+
+module.exports = {
+  async login(req, res) {
+    const { firebaseKey } = req.body;
+    const isEmployee=false;
+    const isCompany=false;
+    //vai a tabela employee ver se existe
+    //Caso nao exista vai a tabela company
+    try{
+      await connection
+      .query("Select * from employee where employee_key=:firebasekey", {
+        replacements: { firebasekey:firebaseKey },
+      })
+      .then(async(results) => {
+        if(results[0].length>0){
+          const userid = results[0][0].employee_key;
+          console.log(userid)
+          const companyid = results[0][0].company_key;
+          return res.json({'status':true,'userid':userid,'companyid':companyid})
+        }else{
+          await connection
+          .query("Select * from company where company_key=:firebasekey", {
+            replacements: { firebasekey:firebaseKey },
+          })
+          .then((results) => {
+            if(results[0].length>0){
+                const companyid = results[0][0].company_key;
+                return res.json({'status':true,'userid':'','companyid':companyid})
+            }else{
+              return res.json({'status':false,'userid':'','companyid':''})
+            }
+          });
+        }
+      });
+    }catch(error){
+      console.log(error)
+      return res.json("Erro interno");
+    }
+  },
+};
